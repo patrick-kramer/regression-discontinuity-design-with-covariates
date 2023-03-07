70bd481f831345ebff6f30856b9ab823b43d2f1a
Patrick Kramer <pk25wyda@studserv.uni-leipzig.de>
2023-03-07 12:10:35 +0100

    Implemented parallel computation

--- RD-with-Generated-Data-Sample_Parallel.R
+++ RD-with-Generated-Data-Sample_Parallel.R
@@ -1,6 +1,7 @@
 # Load packages
 library('mvtnorm')
 library('rdrobust')
+library('parallel')
 
 start_time <- Sys.time()
 
@@ -11,12 +12,12 @@ number_of_montecarlo_replications <- 20
 # 3 - Robust
 type_of_estimator <- 2
 
-coef_vector <- array(NA, c(number_of_montecarlo_replications, 6))
-standard_error_vector <- array(NA, c(number_of_montecarlo_replications, 6))
-ci_length_vector <- array(NA, c(number_of_montecarlo_replications, 6))
-number_for_coverage <- array(0, c(6))
-
-for (n in 1:number_of_montecarlo_replications) {
+perform_rdd <- function(n) {
+  coef_vector <- array(NA, c(6))
+  standard_error_vector <- array(NA, c(6))
+  ci_length_vector <- array(NA, c(6))
+  coverage_vector <- array(0, c(6))
+  
   # Generate finite sample data (score and covariates)
   X <- 2*rbeta(1000, 2, 4)-1
   T <- 0+(X >= 0)
@@ -44,60 +45,65 @@ for (n in 1:number_of_montecarlo_replications) {
   
   # RDD without covariates
   rd_without_covs <- rdrobust(Y, X)
-  standard_error_vector[n, 1] <- rd_without_covs$se[type_of_estimator]
-  coef_vector[n, 1] <- rd_without_covs$coef[type_of_estimator]
-  ci_length_vector[n, 1] <- rd_without_covs$ci[type_of_estimator,2]-rd_without_covs$ci[type_of_estimator,1]
+  standard_error_vector[1] <- rd_without_covs$se[type_of_estimator]
+  coef_vector[1] <- rd_without_covs$coef[type_of_estimator]
+  ci_length_vector[1] <- rd_without_covs$ci[type_of_estimator,2]-rd_without_covs$ci[type_of_estimator,1]
   if (0.02>=rd_without_covs$ci[type_of_estimator,1] && 0.02<=rd_without_covs$ci[type_of_estimator,2]) {
-    number_for_coverage[1] = number_for_coverage[1] + 1
+    coverage_vector[1] = 1
   }
   
   # RDD with 1 covariate
   rd_one_cov <- rdrobust(Y, X, covs = matrix_Z[,1])
-  standard_error_vector[n, 2] <- rd_one_cov$se[type_of_estimator]
-  coef_vector[n, 2] <- rd_one_cov$coef[type_of_estimator]
-  ci_length_vector[n, 2] <- rd_one_cov$ci[type_of_estimator,2]-rd_one_cov$ci[type_of_estimator,1]
+  standard_error_vector[2] <- rd_one_cov$se[type_of_estimator]
+  coef_vector[2] <- rd_one_cov$coef[type_of_estimator]
+  ci_length_vector[2] <- rd_one_cov$ci[type_of_estimator,2]-rd_one_cov$ci[type_of_estimator,1]
   if (0.02>=rd_one_cov$ci[type_of_estimator,1] && 0.02<=rd_one_cov$ci[type_of_estimator,2]) {
-    number_for_coverage[2] = number_for_coverage[2] + 1
+    coverage_vector[2] = 1
   }
   
   # RDD with 10 covariates
   rd_ten_covs <- rdrobust(Y, X, covs = matrix_Z[,1:10])
-  standard_error_vector[n, 3] <- rd_ten_covs$se[type_of_estimator]
-  coef_vector[n, 3] <- rd_ten_covs$coef[type_of_estimator]
-  ci_length_vector[n, 3] <- rd_ten_covs$ci[type_of_estimator,2]-rd_ten_covs$ci[type_of_estimator,1]
+  standard_error_vector[3] <- rd_ten_covs$se[type_of_estimator]
+  coef_vector[3] <- rd_ten_covs$coef[type_of_estimator]
+  ci_length_vector[3] <- rd_ten_covs$ci[type_of_estimator,2]-rd_ten_covs$ci[type_of_estimator,1]
   if (0.02>=rd_ten_covs$ci[type_of_estimator,1] && 0.02<=rd_ten_covs$ci[type_of_estimator,2]) {
-    number_for_coverage[3] = number_for_coverage[3] + 1
+    coverage_vector[3] = 1
   }
   
   # RDD with 30 covariates
   rd_thirty_covs <- rdrobust(Y, X, covs = matrix_Z[,1:30])
-  standard_error_vector[n, 4] <- rd_thirty_covs$se[type_of_estimator]
-  coef_vector[n, 4] <- rd_thirty_covs$coef[type_of_estimator]
-  ci_length_vector[n, 4] <- rd_thirty_covs$ci[type_of_estimator,2]-rd_thirty_covs$ci[type_of_estimator,1]
+  standard_error_vector[4] <- rd_thirty_covs$se[type_of_estimator]
+  coef_vector[4] <- rd_thirty_covs$coef[type_of_estimator]
+  ci_length_vector[4] <- rd_thirty_covs$ci[type_of_estimator,2]-rd_thirty_covs$ci[type_of_estimator,1]
   if (0.02>=rd_thirty_covs$ci[type_of_estimator,1] && 0.02<=rd_thirty_covs$ci[type_of_estimator,2]) {
-    number_for_coverage[4] = number_for_coverage[4] + 1
+    coverage_vector[4] = 1
   }
   
   # RDD with 50 covariates
   rd_fifty_covs <- rdrobust(Y, X, covs = matrix_Z[,1:50])
-  standard_error_vector[n, 5] <- rd_fifty_covs$se[type_of_estimator]
-  coef_vector[n, 5] <- rd_fifty_covs$coef[type_of_estimator]
-  ci_length_vector[n, 5] <- rd_fifty_covs$ci[type_of_estimator,2]-rd_fifty_covs$ci[type_of_estimator,1]
+  standard_error_vector[5] <- rd_fifty_covs$se[type_of_estimator]
+  coef_vector[5] <- rd_fifty_covs$coef[type_of_estimator]
+  ci_length_vector[5] <- rd_fifty_covs$ci[type_of_estimator,2]-rd_fifty_covs$ci[type_of_estimator,1]
   if (0.02>=rd_fifty_covs$ci[type_of_estimator,1] && 0.02<=rd_fifty_covs$ci[type_of_estimator,2]) {
-    number_for_coverage[5] = number_for_coverage[5] + 1
+    coverage_vector[5] = 1
   }
   
   # RDD with optimal covariate
   rd_optimal_cov <- rdrobust(Y, X, covs = Z_times_alpha)
-  standard_error_vector[n, 6] <- rd_optimal_cov$se[type_of_estimator]
-  coef_vector[n, 6] <- rd_optimal_cov$coef[type_of_estimator]
-  ci_length_vector[n, 6] <- rd_optimal_cov$ci[type_of_estimator,2]-rd_optimal_cov$ci[type_of_estimator,1]
+  standard_error_vector[6] <- rd_optimal_cov$se[type_of_estimator]
+  coef_vector[6] <- rd_optimal_cov$coef[type_of_estimator]
+  ci_length_vector[6] <- rd_optimal_cov$ci[type_of_estimator,2]-rd_optimal_cov$ci[type_of_estimator,1]
   if (0.02>=rd_optimal_cov$ci[type_of_estimator,1] && 0.02<=rd_optimal_cov$ci[type_of_estimator,2]) {
-    number_for_coverage[6] = number_for_coverage[6] + 1
+    coverage_vector[6] = 1
   }
   
   cat("Completed run", n, "\n")
+  
+  return(matrix(c(standard_error_vector, coef_vector, ci_length_vector, coverage_vector), 6, 4))
 }
+
+results_list_of_matrices <- mclapply(1:number_of_montecarlo_replications, perform_rdd, mc.cores = 6)
+results_matrix <- simplify2array(results_list_of_matrices)
 
 mean_of_estimator <- c()
 bias <- c()
@@ -108,12 +114,12 @@ coverage <- c()
 results <- matrix(NA, 6, 5, dimnames = list(list("0 Covs", "1 Cov", "10 Covs", "30 Covs", "50 Covs", "Opt. Cov"), list("Bias", "SD", "Avg. SE", "CI Length", "Coverage")))
 
 for (l in 1:6) {
-  mean_of_estimator <- append(mean_of_estimator, mean(coef_vector[,l]))
+  mean_of_estimator <- append(mean_of_estimator, mean(results_matrix[l,2,]))
   bias <- append(bias, mean_of_estimator[l]-0.02)
-  standard_deviation <- append(standard_deviation, sqrt(mean((coef_vector[,l]-mean_of_estimator[l])^2)))
-  standard_error <- append(standard_error, mean(standard_error_vector[,l]))
-  ci_length <- append(ci_length, mean(ci_length_vector[,l]))
-  coverage <- append(coverage, number_for_coverage[l]*100/number_of_montecarlo_replications)
+  standard_deviation <- append(standard_deviation, sqrt(mean((results_matrix[l,2,]-mean_of_estimator[l])^2)))
+  standard_error <- append(standard_error, mean(results_matrix[l,1,]))
+  ci_length <- append(ci_length, mean(results_matrix[l,3,]))
+  coverage <- append(coverage, mean(results_matrix[l,4,])*100)
   
   results[l,] <- c(bias[l], standard_deviation[l], standard_error[l], ci_length[l], coverage[l])
 }
@@ -123,3 +129,4 @@ results
 end_time <- Sys.time()
 ellapsed_time <- end_time - start_time
 cat("Ellapsed time:", ellapsed_time)
+
