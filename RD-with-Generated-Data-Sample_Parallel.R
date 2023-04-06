44fdbf711b4d3933ade5d490e72cf2a06469b7f2
Patrick Kramer <pk25wyda@studserv.uni-leipzig.de>
2023-04-06 11:32:39 +0200

    Outsourced rdd function call

--- RD-with-Generated-Data-Sample_Parallel.R
+++ RD-with-Generated-Data-Sample_Parallel.R
@@ -4,148 +4,8 @@ library('rdrobust')
 library('parallel')
 setwd(getSrcDirectory(function(){})[1])
 source('R/functions.R')
+source('R/RDD_functions.R')
 
-#########################################
-# Define functions                      #
-#########################################
-
-perform_rdd <- function(run) {
-  
-  # Initialize vectors for storing results
-  coef_vector <- array(NA, c(number_of_examinations))
-  standard_error_vector <- array(NA, c(number_of_examinations))
-  ci_length_vector <- array(NA, c(number_of_examinations))
-  coverage_vector <- array(0, c(number_of_examinations))
-  number_of_covs_vector <- array(NA, c(number_of_examinations))
-  selected_covs_vector <- matrix(0, 200, 3)
-  
-  # Generate finite sample data (score and covariates)
-  X <- 2*rbeta(sample_size, 2, 4)-1
-  T <- 0+(X >= 0)
-  sigma_z <- 0.1353
-  sigma_eps <- 0.1295
-  v <- 0.8*sqrt(6)*(sigma_eps)^2/(pi*c(1:200))
-  sigma_z_id_matrix <- diag(x = sigma_z^2, 200)
-  matrix_rows <- matrix(NA, 200, 201)
-  for (k in 1:200) {
-    matrix_rows[k,] <- c(v[k], sigma_z_id_matrix[k,])
-  }
-  variance_matrix <- rbind(c(sigma_eps^2, v), matrix_rows)
-  Z_epsilon <- rmvnorm(sample_size, sigma = variance_matrix)
-  alpha <- 2/(c(1:200))^2
-  matrix_Z <- matrix(Z_epsilon[,2:201], sample_size, byrow = FALSE)
-  vector_alpha <- matrix(alpha, 200, 1, byrow = TRUE)
-  Z_times_alpha <- matrix_Z %*% vector_alpha
-  
-  # Define potential outcomes
-  Y_0 <- Z_epsilon[,1]+0.36+0.96*X+5.47*X^2+15.28*X^3+15.87*X^4+5.14*X^5+0.22*Z_times_alpha
-  Y_1 <- Z_epsilon[,1]+0.38+0.62*X-2.84*X^2+8.42*X^3-10.24*X^4+4.31*X^5+0.28*Z_times_alpha
-  
-  # Define outcome
-  Y <- (1-T)*Y_0+T*Y_1
-  
-  # Set column names of Z
-  colnames(matrix_Z) <- c(1:200)
-
-  # Select covariates
-  Z_002 <- matrix_Z[,compare_correlation(matrix_Z, Y, 0.02)]
-  Z_005 <- matrix_Z[,compare_correlation(matrix_Z, Y, 0.05)]
-  Z_01 <- matrix_Z[,compare_correlation(matrix_Z, Y, 0.1)]
-  Z_02 <- matrix_Z[,compare_correlation(matrix_Z, Y, 0.2)]
-  selected_indices_threshold_method <- compare_correlation(matrix_Z, Y, calculate_correlation_thresholds(matrix_Z, Y, sample_size))
-  Z_calculated_threshold <- matrix_Z[, selected_indices_threshold_method]
-  Z_calculated_threshold_and_deletion_simple <- remove_covs_calculated_threshold(Z_calculated_threshold, Y, sample_size, simple_deletion = TRUE)
-  Z_calculated_threshold_and_deletion <- remove_covs_calculated_threshold(Z_calculated_threshold, Y, sample_size, simple_deletion = FALSE)
-  
-  # Store information about selected covariates
-  for (index in selected_indices_threshold_method) {
-    selected_covs_vector[index, 1] <- selected_covs_vector[index, 1] + 1
-  }
-  indices_after_deletion_simple <- as.numeric(colnames(Z_calculated_threshold_and_deletion_simple))
-  indices_after_deletion <- as.numeric(colnames(Z_calculated_threshold_and_deletion))
-  for (index in indices_after_deletion) {
-    selected_covs_vector[index, 2] <- selected_covs_vector[index, 2] + 1
-  }
-  for (index in indices_after_deletion_simple) {
-    selected_covs_vector[index, 3] <- selected_covs_vector[index, 3] + 1
-  }
-  
-  # Remove covariables for invertibility
-  if (sample_size == 1000) {
-    Z_002 <- remove_covs_with_high_correlation(Z_002, 45)
-    Z_005 <- remove_covs_with_high_correlation(Z_005, 15)
-  }
-  
-  covariate_settings <- list(as.matrix(Z_calculated_threshold),
-                             as.matrix(Z_calculated_threshold_and_deletion),
-                             as.matrix(Z_calculated_threshold_and_deletion_simple),
-                             as.matrix(Z_02),
-                             as.matrix(Z_01),
-                             as.matrix(Z_005),
-                             as.matrix(Z_002),
-                             NA,
-                             as.matrix(matrix_Z[,1]),
-                             matrix_Z[,1:10],
-                             matrix_Z[,1:30],
-                             matrix_Z[,1:50],
-                             Z_times_alpha)
-  
-  if (rdd_library == "robust") {
-    counter <- 1
-    for (covariates in covariate_settings) {
-      if (isTRUE(ncol(covariates)>0)) {
-        rd <- rdrobust(Y, X, covs = covariates)
-      } else {
-        rd <- rdrobust(Y, X)
-      }
-      standard_error_vector[counter] <- rd$se[estimator_type]
-      coef_vector[counter] <- rd$coef[estimator_type]
-      ci_length_vector[counter] <- rd$ci[estimator_type,2]-rd$ci[estimator_type,1]
-      if (0.02>=rd$ci[estimator_type,1] && 0.02<=rd$ci[estimator_type,2]) {
-        coverage_vector[counter] = 1
-      }
-      if (length(ncol(covariates)) == 0) {
-        number_of_covs_vector[counter] <- 0
-      } else {
-        number_of_covs_vector[counter] <- ncol(covariates)
-      }
-      counter <- counter + 1
-    }
-  } else if (rdd_library == "honest") {
-    counter <- 1
-    for (covariates in covariate_settings) {
-      if (isTRUE(ncol(covariates)>0)) {
-        h <- RDHonest::RDHonest(Y~X)$coefficients$bandwidth
-        kernel_weights <- triangular(X/h)/h
-        cov_lm <- lm(covariates~1+X+T+X*T, weights = kernel_weights)
-        v <- cov_lm$residuals
-        sigma_Z <- t(v)%*%(v*kernel_weights)/sample_size
-        sigma_ZY <- colSums(as.matrix(v*as.vector(kernel_weights*Y)))/sample_size
-        gamma_n <- solve(sigma_Z)%*%sigma_ZY
-        Ytilde <- Y-covariates%*%gamma_n
-      } else {
-        Ytilde = Y
-      }
-      rd_ten_covs <- RDHonest::RDHonest(Ytilde~X)
-      
-      standard_error_vector[counter] <- rd_ten_covs$coefficients$std.error
-      coef_vector[counter] <- rd_ten_covs$coefficients$estimate
-      ci_length_vector[counter] <- rd_ten_covs$coefficients$conf.high-rd_ten_covs$coefficients$conf.low
-      if (0.02>=rd_ten_covs$coefficients$conf.low && 0.02<=rd_ten_covs$coefficients$conf.high) {
-        coverage_vector[counter] = 1
-      }
-      if (length(ncol(covariates)) == 0) {
-        number_of_covs_vector[counter] <- 0
-      } else {
-        number_of_covs_vector[counter] <- ncol(covariates)
-      }
-      counter = counter + 1
-    }
-  }
-  results_of_run <- matrix(c(standard_error_vector, coef_vector, ci_length_vector, coverage_vector, number_of_covs_vector), number_of_examinations, 5)
-  
-  return(list(res = results_of_run, sel = selected_covs_vector))
-}
 
 #########################################
 # Run Simulation                        #
@@ -157,24 +17,24 @@ start_time <- Sys.time()
 number_of_montecarlo_replications <- 100
 
 # Sample size n
-sample_size <-1000
+n <-1000
 
 # Library to use for RDD
 # robust - RDRobust
 # honest - RDHonest
-rdd_library <- "honest"
+rdd_library <- "robust"
 
 # Type of estimator (only relevant for RDRobust)
 # 1 - Conventional
 # 2 - Bias-Corrected
 # 3 - Robust
-estimator_type <- 1
+rdrobust_estimator_type <- 1
 
 # Number of examinations
 number_of_examinations <- 13
 
 
-# Perform parallized RDD
+# Perform parallel RDD
 if (Sys.info()['sysname'] == "Windows") {
   cluster <- makeCluster(11)
   load_packages <- parLapply(cluster, 1:length(cluster),
@@ -182,11 +42,21 @@ if (Sys.info()['sysname'] == "Windows") {
                                require('rdrobust')
                                require('mvtnorm')
                              })
-  clusterExport(cluster, c('perform_rdd', 'triangular', 'compare_correlation', 'calculate_correlation_thresholds', 'number_of_examinations'))
-  results_list_of_matrices <- parLapply(cluster, 1:number_of_montecarlo_replications, perform_rdd)
+  clusterExport(cluster, c('perform_rdd', 'triangular', 'calculate_correlation',
+                           'remove_covs_calculated_threshold', 'compare_correlation',
+                           'calculate_correlation_thresholds', 'calculate_correlation_threshold_matrix',
+                           'remove_covs_with_high_correlation'))
+  results_list_of_matrices <- parLapply(cluster, 1:number_of_montecarlo_replications, perform_rdd,
+                                        sample_size = n,
+                                        rdd_library = rdd_library,
+                                        estimator_type = rdrobust_estimator_type)
   stopCluster(cluster)
 } else {
-  results_list_of_matrices <- mclapply(1:number_of_montecarlo_replications, perform_rdd, mc.cores = 11)
+  results_list_of_matrices <- mclapply(1:number_of_montecarlo_replications, perform_rdd,
+                                       sample_size = n,
+                                       rdd_library = rdd_library,
+                                       estimator_type = rdrobust_estimator_type,
+                                       mc.cores = 11)
 }
 results_list <- lapply(results_list_of_matrices, function(x) { x <- x[[1]] })
 selection_list <- lapply(results_list_of_matrices, function(x) { x <- x[[2]] })
